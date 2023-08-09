1bc745c4d70636b4b88013216e39ef63e949b9f4
Lee Tang <i@d0zingcat.dev>
2023-08-09 14:54:36 +0800

    fix
    Signed-off-by: Lee Tang <i@d0zingcat.dev>

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 import os
 
-from fastapi import Depends, FastAPI, Request
+from fastapi import Depends, FastAPI, Request, HTTPException
 from fastapi.middleware.gzip import GZipMiddleware
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
 
 from app.routers import staticfile
@@ -14,8 +14,16 @@ if not os.path.exists(static_file_dir):
     os.mkdir(static_file_dir)
 
 app = FastAPI(dependencies=[])
-# app.add_middleware(GZipMiddleware, minimum_size=1000)
-app.mount('/static', StaticFiles(directory=static_file_dir), name='static')
+app.add_middleware(GZipMiddleware, minimum_size=1000)
+#app.mount('/static', StaticFiles(directory=static_file_dir), name='static')
+# @app.get("/static", response_class=HTMLResponse)
+# def list_files(request: Request):
+#
+#     files = os.listdir(static_file_dir)
+#     files_paths = sorted([{"path": f, "url": f"{request.url._url}/{f}"} for f in files], key=lambda x: x["path"])
+#     return templates.TemplateResponse(
+#         "list_files.html", {"request": request, "files": files_paths}
+#     )
 app.include_router(staticfile.router)
 
 templates = Jinja2Templates(directory="templates")
@@ -29,13 +37,3 @@ async def root():
 @app.get("/ping")
 async def ping():
     return "pong"
-
-
-@app.get("/static", response_class=HTMLResponse)
-def list_files(request: Request):
-
-    files = os.listdir(static_file_dir)
-    files_paths = sorted([{"path": f, "url": f"{request.url._url}/{f}"} for f in files], key=lambda x: x["path"])
-    return templates.TemplateResponse(
-        "list_files.html", {"request": request, "files": files_paths}
-    )
--- app/routers/staticfile.py
+++ app/routers/staticfile.py
@@ -1,7 +1,8 @@
 import os
+from pathlib import Path
 
 from fastapi import APIRouter, HTTPException
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, HTMLResponse
 
 from app.internal.config import STATIC_FILE_DIR, ROOT_DIR
 
@@ -17,3 +18,23 @@ async def download_normal(filename: str):
     response = FileResponse(filepath)
     response.headers["Content-Disposition"] = f"attachment; filename={filename}"
     return response
+
+
+@router.get("/static/{path:path}")
+async def serve_file(path: str):
+    full_path = Path("static") / path
+    if full_path.is_file():
+        return FileResponse(full_path)
+    elif full_path.is_dir():
+        return HTMLResponse(generate_directory_listing(full_path))
+    else:
+        raise HTTPException(status_code=404, detail="File not found")
+
+
+def generate_directory_listing(directory_path):
+    listing = "<h1>Directory Listing</h1>"
+    listing += "<ul>"
+    for item in directory_path.iterdir():
+        listing += f"<li><a href='{item.name}'>{item.name}</a></li>"
+    listing += "</ul>"
+    return listing
